2e3e73dba2962e1f0ec3cc66fd6b859479b7fdd7
Daiana Barbosa Soares <daiana.soares@b2wdigital.com>
2024-12-23 23:39:46 -0300

    [refactor]  refactoriong environment configurações

--- cmd/api-server/main.go
+++ cmd/api-server/main.go
@@ -2,13 +2,54 @@ package main
 
 import (
 	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/sirupsen/logrus"
 
 	"project-golang/internal/fx/server"
+	//"github.com/spf13/viper"
 )
+
+var (
+	QUEUE_URL string
+)
+
+func LoadEnv() {
+	QUEUE_URL = os.Getenv("QUEUE_URL")
+
+}
+
+func init() {
+	if err := godotenv.Load("../../configs/prod.env"); err != nil && !os.IsNotExist(err) {
+		logrus.WithFields(logrus.Fields{
+			"application": "migration-worker",
+			"brand":       "b2w",
+		}).Fatalf("Error loading .env - %s", err.Error())
+	}
+	LoadEnv()
+
+	//awssqs.Configure(os.Getenv("QUEUE_VPC_ENDPOINTS"))
+
+	//cassandra.OpenDatabases()
+}
 
 func main() {
 	fmt.Println("iniciando aplicação")
 	fmt.Println("server.Start")
+	var2 := os.Getenv("QUEUE_URL")
+	fmt.Println(" estou imprimindo: "+var2)
+
+	// viper.AddConfigPath("../../configs/worker")
+	// viper.SetConfigName("development") // Nome do arquivo sem extensão
+	// viper.SetConfigType("yaml")
+
+	// viper.AddConfigPath("../../configs")
+	// viper.SetConfigName("default") // Nome do arquivo sem extensão
+	// viper.SetConfigType("yaml")
+	// viper.AddConfigPath("../../configs")
+	// viper.SetConfigName("rest") // Nome do arquivo sem extensão
+	// viper.SetConfigType("yaml")
 
 	server.Start()
 
